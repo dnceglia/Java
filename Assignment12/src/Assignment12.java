b6036714faf3658412dc849fb38e9c44f654e4d4
dnceglia <d.n.ceglia@hotmail.com>
2017-11-17 20:43:45 -0500

    Assignment 12 upload
    upload try/catch

---
+++ Assignment12/src/Assignment12.java
@@ -0,0 +1,26 @@
+
+public class Assignment12 {
+
+    public static void main(String[] args) {
+        
+        int a=0,b=0,c=0;
+        
+        System.out.println("Assignment 12"); 
+        
+        a=10;
+        
+        try {
+            
+            c = a/b;   
+            
+        }
+            
+        catch(ArithmeticException e){
+                        
+            System.out.println("We are exiting gracefully");
+                        
+        }
+            
+    }
+    
+}
